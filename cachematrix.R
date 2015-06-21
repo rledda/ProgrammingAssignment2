8bebe5ee824a0abd80846f7804d60683dc40f257
Roberto Ledda <ledda.roberto@gmail.com>
2015-06-21 11:32:49 +0200

    Update cachematrix.R
    place the functions 'makeCacheMatrix' and 'cacheSolve'

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,45 @@
-## Put comments here that give an overall description of what your
-## functions do
+## La funzione 'makeCacheMatrix' consente l'archiviazione nella cache di una matrice e della sua matrice inversa 
+## mediante la creazione di una 'lista' di 4 funzioni (set, get, set.inv, get.inv)
+## sfruttando l'operatore '<<', il quale consente di assegnare un valore definito nell'ambiente di lavoro ad un oggetto definito in un ambiente distinto.
 
-## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix <- function(m = matrix()) { # 'm' e' la matrice iniziale di input
+        
+        m.inv <- NULL # in 'm.inv' viene archiviata la matrice inversa di (inizializzata con valore NULL)
+        # 'set' e' una funzione che consente di gestire 2 matrici, una come argomento iniziale ed una nella memoria cache
+        # ad es. makeCacheMatrix(m) - qui opero con la prima matrice 'm'
+        # ad es. makeCacheMatrix$set(m2) - qui opero con la seconda matrice 'm2'
+        set <- function(m2) { 
+                m <<- m2 # assegno a 'm' il valore della matrice nuova 'm2'
+                m.inv <<- NULL # inizializzo m.inv con valore NULL
+        }
+        
+        get <- function() m # assegno a 'get' la matrice 'm' di input
+        set.inv <- function(inv) m.inv <<- inv # imposto la matrice inversa
+        get.inv <- function() m.inv # restituisco la matrice inversa, da calcolarsi con la funzione 'cacheSolve'
+        # costruisco una lista che contiene 4 funzioni
+        # o <- makeCacheMatrix(m)
+        # o$set(m2) # per cambiare la matrice
+        # o$get # per recuperare la  matrice impostata 'm'
+        # o$set.inv # per impostare la matrice inversa di 'm2', calcolata con la funzione 'cacheSolve'
+        # o$get.inv # per recuperare la matrice inversa di 'm', calcolata con la funzione 'cacheSolve'
+        list(set = set, get = get, set.inv = set.inv, get.inv = get.inv)
 }
 
 
-## Write a short comment describing this function
+## La funzione cacheSolve calcola la matrice inversa della matrice archiviata nella 'lista' creata con la funzione 'makeCacheMatrix'
+## 'cacheSolve' verifica se la matrice inversa e' gia' stata calcolata;
+# se cio' e' vero, recupera la matrice inversa dalla cache, 
+## altrimenti calcola la matrice inversa con la funzione 'solve' ed la carica nella cache
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function(o, ...) { #l'argomento 'o' costituisce l'output della funzione makeCacheMatrix
+        m <- o$get.inv() # recupero la matrice inversa archiviata in 'o'
+        # verifico se la matrice inversa non e' stata ancora calcolata - vedi comando "m.inv <- NULL" nella funzione 'makeCacheMatrix'
+        if(!is.null(m)) { # se la matrice inversa e' stata calcolata, cioe' se o$get.inv() non e' NULL
+                message("Recupero in corso dei dati dalla cache....") #recupero la matrice inversa dalla cache
+                return(m) # restituisco la matrice inversa
+        }
+        matr <- o$get() # se la matrice inversa non e' stata calcolata, recupero la matrice da o$get()
+        m <- solve(matr) # calcolo la matrice inversa 
+        o$set.inv(m) # assegno la matrice inversa a o$set.inv
+        m # restituisco la matrice inversa
 }
